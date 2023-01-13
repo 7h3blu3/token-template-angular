6d8e0eb76b85a115ee795baba52115e6fa6f5335
Milen Deyanov <milen.krasimirov.deyanov@gmail.com>
2023-01-13 12:51:51 +0200

    mat-card mobile responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "http://localhost:3000"
+  apiUrl: "http://localhost:3000"
   // apiUrl: 'https://nodejs-md--assessment-backend.herokuapp.com'
-  apiUrl: "https://crimson-phantom-sidecar.glitch.me"
+  // apiUrl: "https://crimson-phantom-sidecar.glitch.me"
 };
